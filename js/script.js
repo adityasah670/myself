5c0cfef8524b56ed9f2bde06367679bd4bab241b
adityasah670 <132773333+adityasah670@users.noreply.github.com>
2023-05-30 11:29:44 +0530

    script.js

--- js/script.js
+++ js/script.js
@@ -1,25 +1,3 @@
-function nightbut(){
-    document.getElementById('body').style.backgroundColor=''
-    document.getElementById('body').style.color=''
-    document.getElementById('nighton').style.display='none'
-    document.getElementById('dayton').style.display='block'
-    document.getElementById('head').style.color=''
-    document.getElementById('navi').style.color=''
-    document.getElementById('about').style.color=''
-    document.getElementById('clock').style.backgroundColor=''
-    document.getElementById('clock').style.color=''
-}
-function daybut(){
-    document.getElementById('body').style.backgroundColor='white'
-    document.getElementById('body').style.color='black'
-    document.getElementById('nighton').style.display='block'
-    document.getElementById('dayton').style.display='none'
-    document.getElementById('head').style.color='white'
-    document.getElementById('navi').style.color='black'
-    document.getElementById('about').style.color='black'
-    document.getElementById('clock').style.backgroundColor='white'
-    document.getElementById('clock').style.color='black'
-}
 setInterval(showTime, 1000);
 function showTime() {
     let time = new Date();
@@ -48,3 +26,29 @@ function showTime() {
             .innerHTML = currentTime;
 }
 showTime();
+let slideIndex = 1;
+            showSlides(slideIndex);
+            
+            function plusSlides(n) {
+              showSlides(slideIndex += n);
+            }
+            
+            function currentSlide(n) {
+              showSlides(slideIndex = n);
+            }
+            
+            function showSlides(n) {
+              let i;
+              let slides = document.getElementsByClassName("mySlides");
+              let dots = document.getElementsByClassName("dot");
+              if (n > slides.length) {slideIndex = 1}    
+              if (n < 1) {slideIndex = slides.length}
+              for (i = 0; i < slides.length; i++) {
+                slides[i].style.display = "none";  
+              }
+              for (i = 0; i < dots.length; i++) {
+                dots[i].className = dots[i].className.replace(" active", "");
+              }
+              slides[slideIndex-1].style.display = "block";  
+              dots[slideIndex-1].className += " active";
+            }
